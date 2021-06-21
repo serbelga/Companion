a651596ba1a35ca16648603d2f5b7eb21f4a34fc
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2021-06-21 22:47:47 +0200

    Upgrade dependencies. Rollback `Moved launchActivity extension function from Activity to Context`.

--- companion/src/main/java/dev/sergiobelda/android/companion/app/Activity.kt
+++ companion/src/main/java/dev/sergiobelda/android/companion/app/Activity.kt
@@ -18,6 +18,7 @@ package dev.sergiobelda.android.companion.app
 
 import android.app.Activity
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 
@@ -42,3 +43,26 @@ inline fun <reified A : Activity> Activity.launchActivityForResult(
     options: Bundle? = null,
     block: Intent.() -> Unit = {}
 ) = startActivityForResult(Intent(this, A::class.java).apply(block), requestCode, options)
+
+/**
+ * Launch a new Activity and runs the Intent body that will be passed to the new Activity.
+ *
+ * ```
+ * launchActivity<MyActivity>()
+ *
+ * launchActivity<MyActivity> {
+ *      putExtra(...)
+ * }
+ * ```
+ *
+ * @param options Additional options for how the Activity should be started.
+ * @param block Intent body to start.
+ * @throws [ActivityNotFoundException].
+ */
+inline fun <reified A : Activity> Context.launchActivity(
+    options: Bundle? = null,
+    block: Intent.() -> Unit = {}
+) = startActivity(
+    Intent(this, A::class.java).apply(block),
+    options
+)
--- companion/src/main/java/dev/sergiobelda/android/companion/content/Context.kt
+++ companion/src/main/java/dev/sergiobelda/android/companion/content/Context.kt
@@ -16,40 +16,13 @@
 
 package dev.sergiobelda.android.companion.content
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
-import android.os.Bundle
 import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
 import androidx.annotation.RequiresApi
-
-/**
- * Launch a new Activity and runs the Intent body that will be passed to the new Activity.
- *
- * ```
- * launchActivity<MyActivity>()
- *
- * launchActivity<MyActivity> {
- *      putExtra(...)
- * }
- * ```
- *
- * @param options Additional options for how the Activity should be started.
- * @param block Intent body to start.
- * @throws [ActivityNotFoundException].
- */
-inline fun <reified A : Activity> Context.launchActivity(
-    options: Bundle? = null,
-    block: Intent.() -> Unit = {}
-) = startActivity(
-    Intent(this, A::class.java).apply(block),
-    options
-)
 
 /**
  * The version name of the package application, as specified by the <manifest>
